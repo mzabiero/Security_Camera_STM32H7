23e343950bdbe91b14eb10e598b2cf794a354911
Maks Z <1178463@pw.edu.pl>
2025-12-02 13:58:46 +0100

    While debugging it restarts all the time, but with normal power supply it works properly -> after initialization camera streams into LCD, without any additional funcitonality

---
+++ Devices_ctrl/Inc/Camera_ctrl.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "camera.h"
+#include "main.h"
+
+// Struktura błędów kamery
+typedef enum {
+	CAM_OK			= 0,
+	CAM_ERR			= 1,
+	CAM_INIT_ERR	= 2,
+	CAM_I2C_ERR		= 3
+} Cam_Status_t;
+
+// Struktura ustawień kamery
+typedef struct {
+    uint8_t brightness; // 0-100
+    uint8_t contrast;   // 0-100
+    uint8_t night_mode; // 0: Off, 1: On (High Gain)
+    uint8_t motion_detect_enabled;
+} Camera_Settings_t;
+
+extern Camera_Settings_t AppCamSettings;
+
+// Funkcje do obsługi kamery
+Cam_Status_t Camera_Init(void);
+Cam_Status_t Camera_StartStream(uint32_t *buffer_addr, uint32_t buffer_size);
+Cam_Status_t Camera_StopStream(void);
+
+// Funkcje pomocnicze/ wrappery dla biblioteki
+Cam_Status_t Camera_SetBrightness(uint8_t val);
+Cam_Status_t Camera_SetContrast(uint8_t val);
+Cam_Status_t Camera_SetNightMode(uint8_t enable);
+
---
+++ Devices_ctrl/Src/Camera_ctrl.c
@@ -0,0 +1,10 @@
+/*
+ * Camera_ctrl.c
+ *
+ *  Created on: Dec 2, 2025
+ *      Author: mzabiero
+ */
+
+#include "Camera_ctrl.h"
+
+
